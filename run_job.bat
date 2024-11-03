c0d853339b8a05d14a778eacd22ad94bf3dc6a38
gG656568 <jiadongzh31@gmail.com>
2024-11-03 19:01:07 +0800

    Update run_job.bat

--- run_job.bat
+++ run_job.bat
@@ -2,7 +2,22 @@
 cd %~dp0
 cd..
 cd job
+echo ------整体作业，支持批量作业------
+echo 当前时间作业 conda run -n gupiao python execute_daily_job.py
+conda run -n gupiao python execute_daily_job.py 2023 - 03 - 01
+conda run -n gupiao python execute_daily_job.py 2023 - 03 - 01,2023 - 03 - 02
+conda run -n gupiao python execute_daily_job.py 2023 - 03 - 01 2023 - 03 - 21
+echo ------单功能作业，除了创建数据库，其他都支持批量作业------
+conda run -n gupiao python init_job.py
+conda run -n gupiao python selection_data_daily_job.py
+conda run -n gupiao python basic_data_daily_job.py
+conda run -n gupiao python backtest_data_daily_job.py
+conda run -n gupiao python basic_data_other_daily_job.py
+conda run -n gupiao python indicators_data_daily_job.py
+conda run -n gupiao klinepattern_data_daily_job.py
+conda run -n gupiao python strategy_data_daily_job.py
+conda run -n gupiao python backtest_data_daily_job.py
 echo ------正在执行作业中，请等待------
-echo conda run -n gupiao python execute_daily_job.py
+conda run -n gupiao python execute_daily_job.py
 pause
 exit
